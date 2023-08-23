f14582977491df0cb5b4b568cd0a7332c3d02481
dacortes <dannyel.cortesr@gmail.com>
2023-08-23 16:23:54 +0200

    updt➜no memory leaks

--- src/built-ins/export.c
+++ src/built-ins/export.c
@@ -6,7 +6,7 @@
 /*   By: dacortes <dacortes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 14:05:24 by dacortes          #+#    #+#             */
-/*   Updated: 2023/08/23 15:54:59 by dacortes         ###   ########.fr       */
+/*   Updated: 2023/08/23 16:22:51 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ int	is_it_replaceable(t_env *env, t_aux tmp)
 	{
 		if (ft_strncmp(find->key, tmp._key, -1) == 0)
 		{
-			if (find->eql && ft_strncmp(find->val, "", sizeof("")) != 0)
+			if (find->val)
 				free(find->val);
 			find->val = ft_strdup(tmp._val);
 			find->eql = tmp.eql;
@@ -52,7 +52,6 @@ void	put_value_null(t_aux *tmp, char *inp)
 {
 	if (tmp->eql && !inp[tmp->key + 1])
 	{
-		ft_printf(R"_val%s--->%p\n"E, tmp->_val, tmp->_val);
 		free(tmp->_val);
 		tmp->_val = ft_strdup("");
 		if (!tmp->_val)
@@ -67,7 +66,6 @@ void	put_value_null(t_aux *tmp, char *inp)
 	}
 }
 
-/* queda un leak de m emoria */
 int	_export(t_mini *sh, char *inp)
 {
 	t_aux tmp;
