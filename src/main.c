e641242643aab06da03dcb9c5602fe942ce25ed0
dacortes <dannyel.cortesr@gmail.com>
2023-08-23 12:48:33 +0200

    add➜clear main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: dacortes <dacortes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/30 11:40:11 by dacortes          #+#    #+#             */
-/*   Updated: 2023/08/23 12:30:12 by dacortes         ###   ########.fr       */
+/*   Updated: 2023/08/23 12:48:02 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,29 @@ int	msg_error(int e, int exit_, char *cm)
 		perror("mini");
 	return (exit_);	
 }
+/*
+	funcion para limpiar el env temporalmente 
+	hay que hacer una funcion que limpie y imprima los msg de error cuando falla
+*/
+int	clear(t_mini *sh)
+{
+	t_env	*rm;
+	t_env	*tmp;
+
+	rm = sh->env;
+	while (rm)
+	{
+		tmp = rm;
+		if (rm->key)
+			free(rm->key);
+		if (rm->val)
+			free(rm->val);
+		rm = rm->next;
+		free(tmp);
+	}
+	free(sh);
+	return (SUCCESS);
+}
 
 int	main(int ac, char **av, char **env)
 {
@@ -34,5 +57,6 @@ int	main(int ac, char **av, char **env)
 	sh->env = NULL;
 	init_env(sh, env);
 	_env(sh->env);
+	clear(sh);
 	return (SUCCESS);
 }
