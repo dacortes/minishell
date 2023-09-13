59bd2d4fa1456b6981b08d15afaecf65d5ef056f
dacortes <dannyel.cortesr@gmail.com>
2023-09-13 14:32:09 +0200

    [IMP] Modularity is applied in the ft_exti function

--- src/built-ins/exit.c
+++ src/built-ins/exit.c
@@ -6,17 +6,32 @@
 /*   By: dacortes <dacortes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 14:05:18 by dacortes          #+#    #+#             */
-/*   Updated: 2023/09/06 14:02:33 by dacortes         ###   ########.fr       */
+/*   Updated: 2023/09/13 14:31:29 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/shell_mini.h"
 
-int	ft_exit(t_line **ln, t_mini *sh, char **argv, int argc)
+static void	loop_ext(t_line **ln, t_mini *sh, char **argv)
 {
 	int	i;
 
 	i = 0;
+	while (argv[1][i])
+	{
+		if (!ft_isdigit(argv[1][i]) && argv[1][i] != '-' \
+			&& argv[1][i] != '+')
+		{
+			clear_ln(ln);
+			clear(sh);
+			exit (msg_error(EX, 255, argv[1]));
+		}
+		i++;
+	}
+}
+
+int	ft_exit(t_line **ln, t_mini *sh, char **argv, int argc)
+{
 	if (argc == 1)
 	{
 		clear_ln(ln);
@@ -26,17 +41,7 @@ int	ft_exit(t_line **ln, t_mini *sh, char **argv, int argc)
 	}
 	else if (argc >= 2)
 	{
-		while (argv[1][i])
-		{
-			if (!ft_isdigit(argv[1][i]) && argv[1][i] != '-' \
-				&& argv[1][i] != '+')
-			{
-				clear_ln(ln);
-				clear(sh);
-				exit (msg_error(EX, 255, argv[1]));
-			}
-			i++;
-		}
+		loop_ext(ln, sh, argv);
 		if (argc > 2)
 			return (msg_error(E_ARG, 1, "exit"));
 		exit (ft_atoi(argv[1]));
