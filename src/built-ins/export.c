1b9b9e6235fe28fb24f7e1e776f685c27528c903
dacortes <87203430+dacortes@users.noreply.github.com>
2024-07-23 14:38:02 +0000

    [RM] copys lib, libft, src, README....

--- src/built-ins/export.c
+++ src/built-ins/export.c
@@ -3,12 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dacortes <dacortes@student.42.fr>          +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 15:21:04 by dacortes          #+#    #+#             */
-/*   Updated: 2024/07/20 15:21:13 by dacortes         ###   ########.fr       */
+/*   Updated: 2024/07/23 09:01:38 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+int	check_key(char *key)
+{
+	int	i;
+
+	if (!key)
+		return (FALSE);
+	if (!ft_isalpha(key[0]) && key[0] != '_')
+		return (FALSE);
+	i = 1;
+	while (key[i] && key[i] != '=')
+	{
+		if (!ft_isalnum(key[i]) && !ft_isalpha(key[i]))
+			return (FALSE);
+		i++;
+	}
+	return (TRUE);
+}
