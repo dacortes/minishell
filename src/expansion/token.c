b8a0e375ce18a3252c3def70956052066b0e3b52
Dannyel Cortés Romero <dacortes@car12s5.42barcelona.com>
2024-08-12 17:11:27 +0200

    [FIX] expansor: pointer iterator:list

--- src/expansion/token.c
+++ src/expansion/token.c
@@ -6,7 +6,7 @@
 /*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 12:57:10 by codespace         #+#    #+#             */
-/*   Updated: 2024/08/09 10:20:22 by codespace        ###   ########.fr       */
+/*   Updated: 2024/08/12 17:09:30 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ t_basic	*connect(t_minishell *mini, t_basic *node, t_basic *new, t_basic **init)
 		new->prev = node->prev;
 		node->prev->next = new;
 		new->data.token->has_space = node->data.token->has_space;
-		*init = new;
+		*init = new->prev;
 	}
 	last = get_last_expand(new);
 	if (last)
@@ -99,22 +99,30 @@ int	expand_token(t_minishell *mini, t_basic **start, t_basic *end)
 	t_token	*token;
 	t_basic *init;
 	t_basic *iter;
+	t_basic	*first;
+	int		flag;
 	
 	init = NULL;
 	iter = *start;
+	flag = FALSE;
 	while (iter != end)
 	{
 		token = iter->data.token;
 		if (token->type == EXPAN && token->is_quote == DOUBLE_QUOTES)
 			token->content = expansion(mini, token->content);
 		else if (token->type == EXPAN && token->is_quote == FALSE)
+		{
 			iter = wordspliting(mini, iter, &init);
+			if (flag == FALSE)
+				first = init;
+			flag = TRUE;
+		}
 		if (!iter)
 			break ;
 		iter = iter->next;
 	}
 	if (init && init->prev)
-		*start = init->prev;
+		*start = first;
 	else if (init && !init->prev)
 		*start = init;
 	return (EXIT_SUCCESS);
